6f06ab32724e0e5c17775f552d16a4b78bfb1e09
Michael Eisenbraun <eisenbm@algonquincollege.com>
2017-08-28 15:53:01 -0400

    Moves all files to examples directory updates file names

--- examples/error_handling_and_debugging.php
+++ examples/error_handling_and_debugging.php
@@ -1,36 +1,28 @@
 <?php
-	require "includes/debugbar.php";
+	/*
+	* Display the color and fruit.
+	* The text should match the color.
+	*/
+	$colors = ['red', 'blue', 'green', 'yellow'];
+	$fruits = ['apples', 'berries', 'grapes', 'bananas'];
 
-	//ini_set('display_errors','on');
+	for ($i = 0; $i < count($colors); $i++) {
+		echo '<h2 style="background-color:'.$colors[$i].'; padding:5px;">'.$colors[$i].' '.$fruits[$i].'<h2>';
+	}
 
+	/*
+  * Display only even numbers from 1 to 10
+	*/
+	echo '<ul>';
+	for ($i = 1; $i < 10; $i++) {
+		$remainder = $i%2;
 
-	register_shutdown_function(function() {
-		$success_msg = '<h1 style="position: fixed; top: 0; left: 0; width: 100%; background: #EFD6D6; color: #993031; padding: 15px; margin: 0px;">%s</h1>';
-		$error_msg = '<h1 style="position: fixed; top: 0; left: 0; width: 100%; background:#D8EECE; color: #2E662C; padding: 15px; margin: 0px;">%s</h1>';
-
-		$error = error_get_last();
-
-		if (!empty($error)) {
-			echo sprintf($error_msg, 'There are some errors on the page.');
-		} else {
-			echo sprintf($success_msg, 'Great! You have successfully found all the errors.');
+		if ($remainder == 0) {
+			echo '<li>'.$i.'</li>';
 		}
-	});
+	}
+	echo '</ul>';
 
-	//var_dump((object) range(0, 1000000000));
-
-	$name = "John Smith";
-
-	$bio = "I am a boring person, but I still talk a lot. Blah, blah, blah.";
-
-	echo "<h1 style=\"margin: 100px 0 0 0;\">My name is " . $name . ".";
-	$bio;
-
-	$names = [
-		'Jack',
-		'Dick',
-		'Harry'
-	];
-
-
-	echo "";
+	/*
+  * Display the student's le
+	*/
