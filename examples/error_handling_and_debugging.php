54d08fbce420337507d270a1e1e4fa40bec7549c
Michael Eisenbraun <eisenbm@algonquincollege.com>
2017-09-01 09:17:32 -0400

    Updates file to intentially cause syntax and logic errors

--- examples/error_handling_and_debugging.php
+++ examples/error_handling_and_debugging.php
@@ -3,25 +3,25 @@
 	* Display the color and fruit.
 	* The text should match the color.
 	*/
-	$colors = ['red', 'blue', 'green', 'yellow'];
-	$fruits = ['apples', 'berries', 'grapes', 'bananas'];
+	$colors = ['red', 'blue', 'green', 'yellow']
+	$fruits = ['apples', 'berries', 'grapes', 'bananas'
 
 	for ($i = 0; $i < count($colors); $i++) {
-		echo '<h2 style="background-color:'.$colors[$i].'; padding:5px;">'.$colors[$i].' '.$fruits[$i].'<h2>';
-	}
+		echo '<h2 style="background-color:'.$fruits[$i].'; padding:5px;">'.$colors[$i].' '.$fruits[$i].'<h2>';
+
 
 	/*
   * Display only even numbers from 1 to 10
 	*/
-	echo '<ul>';
+	echo '<ul>'
 	for ($i = 1; $i < 10; $i++) {
 		$remainder = $i%2;
 
-		if ($remainder == 0) {
-			echo '<li>'.$i.'</li>';
-		}
+		if ($remainder = 0) {
+			echo '<li>'.$i.'</li>'
+		
 	}
-	echo '</ul>';
+	echo '</ul>'
 
 	/*
   * Display the student's le
